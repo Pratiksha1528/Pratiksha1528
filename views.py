bfae98b06cffeff90081e7b2c7c727846af0a8bd
Pratiksha1528 <gawarepratiksha2802@gmail.com>
2024-12-16 17:05:27 +0530

    Add files via upload

---
+++ views.py
@@ -0,0 +1,51 @@
+from django.shortcuts import render
+
+# Create your views here.
+def homepage(request):
+    return render(request,"login.html")
+def signup(request):
+    return render(request,"signup.html")
+def signup(request):
+    return render(request,"navigation.html")
+def signup(request):
+    return render(request,"mens_wear.html")
+def signup(request):
+    return render(request,"jackets_and_coats.html")
+def signup(request):
+    return render(request,"shirts.html")
+def signup(request):
+    return render(request,"tshirts.html")
+def signup(request):
+    return render(request,"footwear.html")
+def signup(request):
+    return render(request,"watches.html")
+def signup(request):
+    return render(request,"womwnswear.html")
+def signup(request):
+    return render(request,"kidswear.html")
+def signup(request):
+    return render(request,"about.html")
+def signup(request):
+    return render(request,"watches.html")
+def signup(request):
+    return render(request,"tshirtscombo.html")
+def signup(request):
+    return render(request,"saree.html")
+def signup(request):
+    return render(request,"jewellary.html")
+def signup(request):
+    return render(request,"footwear1.html")
+def signup(request):
+    return render(request,"dresses.html")
+def signup(request):
+    return render(request,"jacketsandshirts.html")
+def signup(request):
+    return render(request,"footwear2.html")
+def signup(request):
+    return render(request,"contact.html")
+def signup(request):
+    return render(request,"traditionalwear.html")
+def signup(request):
+    return render(request,"traditionalwear1.html")
+
+
